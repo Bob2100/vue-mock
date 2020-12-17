33ba9cc0ca2f1123fc63ce996ecdd3eb3012c7d8
Bob <bob_w@qq.com>
2020-12-17 17:20:37 +0800

    监听机制

---
+++ mvue.js
@@ -0,0 +1,35 @@
+class MVue {
+
+  $data;
+
+  constructor(options) {
+    this.$data = options.data;
+    this.observe(this.$data);
+  }
+
+  observe($data) {
+    if (!$data || typeof $data != 'object') {
+      return;
+    }
+    Object.keys($data).forEach(key => {
+      this.defineReactive($data, key, $data[key]);
+    });
+  }
+
+  defineReactive($data, key, value) {
+    Object.defineProperty($data, key, {
+      enumerable: true,
+      configurable: true,
+      get() {
+        return value;
+      },
+      set(newValue) {
+        if (newValue === value) {
+          return;
+        }
+        value = newValue;
+        console.log('数据变化，发出通知');
+      }
+    })
+  }
+}
